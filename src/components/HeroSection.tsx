db2652b8d6fc467b5db8fecfbae31e215d7b1694
michaelvipin <xlmvipin@gmail.com>
2026-01-09 21:23:10 +0530

    changed hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import heroImage from "@/assets/hero-beach.jpg";
+import heroImage from "@/assets/hero-beach.mp4";
 
 const HeroSection = () => {
   const scrollToSection = (id: string) => {
